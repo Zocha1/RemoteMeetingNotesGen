b82721cadfcf9c5e1d9a2c3c18006531ced9dd7c
pawel-rus <pawelrus.637@gmail.com>
2024-12-04 18:23:43 +0100

    check if db already exists

--- backend/flask_api/main.py
+++ backend/flask_api/main.py
@@ -1,9 +1,10 @@
 from app import create_app
 from database.db import init_db
-
+import os
 app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)  
-    init_db()
-    print("Database initialized!")
+    if not os.path.exists('mydb.db'):
+        init_db()
+        print("Database initialized!")
