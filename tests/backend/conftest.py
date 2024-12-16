54231e51a228f5b1dfc652c62cf2ca401ff808d5
pawel-rus <pawelrus.637@gmail.com>
2024-12-16 22:48:38 +0100

    test adding meeting to db

--- tests/backend/conftest.py
+++ tests/backend/conftest.py
@@ -6,7 +6,7 @@ import pytest
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../backend/flask_api')))
 
 from app import create_app  # Import your Flask app creation function
-from database.db import init_db  # Import database initialization 
+from app.models import db # Import database initialization 
 
 @pytest.fixture
 def app():
@@ -14,7 +14,14 @@ def app():
     app = create_app() 
     app.config['TESTING'] = True
     app.config['UPLOAD_FOLDER'] = '/tmp/uploads'  # Set a temp folder for uploads
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'  # Use an in-memory db
+
+    with app.app_context():
+      db.create_all() # Create tables
     yield app
+    
+    with app.app_context():
+        db.drop_all() # Drop all tables after test
 
 @pytest.fixture
 def client(app):
